37d5fbc34c897766d8a461d4d80ae48445c19b1e
sahil <sahilbhutani7@gmail.com>
2018-02-14 19:50:25 -1000

    removed static link to mongo db

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
-
 const app            = express();
 
 const port = 3000;
-const MONGO_URL = 'mongodb://dbuser:fightOn@ds235708.mlab.com:35708/notesdb';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
